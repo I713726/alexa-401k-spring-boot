3612e6ebcd29cfa5426b0c34a55d893cfd72c227
Armen <rissmiller.a@husky.neu.edu>
2018-07-31 14:02:30 -0400

    documentation

--- src/main/java/voya401k/ResponseSpeechBuilder.java
+++ src/main/java/voya401k/ResponseSpeechBuilder.java
@@ -1,5 +1,9 @@
 package voya401k;
 
+/**
+ * This interface is part of an idea for creating varied responses each time a question is asked, to make the experience
+ * more human. However, I am not sure if this is going to be the interface I ultimately end up using.
+ */
 public interface ResponseSpeechBuilder {
     public String getResponseSpeech(int questionNumber, VoyaRequestType requestType, VoyaIntentType intentType,
                                     String locale, VoyaUserDataObject userDataObject);
